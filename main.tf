83adf6d3316eac9aed018b557ef075e3029b5aec
Kamlesh Ray <kamleshray26@gmail.com>
2024-08-30 15:05:21 +0530

    added

--- main.tf
+++ main.tf
@@ -4,10 +4,10 @@ provider "aws" {
 
 resource "aws_s3_bucket" "my_bucket" {
   bucket = "my-unique-bucket-name-12345" # Bucket names must be unique across all existing bucket names in Amazon S3
-  acl    = "private" # Defines who can access the bucket. Options include: private, public-read, public-read-write, aws-exec-read, authenticated-read, and log-delivery-write.
+  acl    = "private"                     # Defines who can access the bucket. Options include: private, public-read, public-read-write, aws-exec-read, authenticated-read, and log-delivery-write.
 
   tags = {
-    Name                        = "My S3 Bucket"
-    Environment             = "Development"
+    Name        = "My S3 Bucket"
+    Environment = "Development"
   }
 }
